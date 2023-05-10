8eee950c3c1b2a5a2c547772f1906f87b5bfff9f
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2023-05-10 15:25:51 +0100

    main: add program component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-// import "@coreui/coreui/dist/css/coreui.min.css";
 import Pages from "./components/pages/Pages";
 
 function App() {
--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./programs.css";
 import { programs } from "../data/Program";
-// import Anniversary from "../Home/Anniversary/Anniversary";
 import FirstTimer from "../Home/FirstTimer/FirstTimer";
 
 function Programs() {
@@ -9,14 +8,21 @@ function Programs() {
     <>
       <section className="program flex" id="program">
         <div className="container">
+          <h2 className="title">Join us on any of our weekly programs...</h2>
           {programs.map((val, index) => (
             <div className="program-box">
-              <div className="icon">
-                <p>Icon</p>
-              </div>
+              <div className="icon">{val.letter}</div>
               <div className="text">
                 <h3>{val.name}</h3>
-                <p>Program description</p>
+                <p>{val.desc}</p>
+                <div className="date-time">
+                  <i class="fa-solid fa-calendar-days"></i>
+                  <p style={{ marginTop: 10 }}>Day: Every {val.day}</p>
+                </div>
+                <div className="date-time">
+                  <i class="fa-solid fa-clock"></i>
+                  <p style={{ marginTop: 6 }}>Time: {val.time}</p>
+                </div>
               </div>
             </div>
           ))}
